5eecaf009c3f5a0ef38551c158e456c8ccc0e5d1
Dennis Witt <witt.dennis@outlook.com>
2022-06-08 20:54:18 +0200

    added emotion and sized fontawesome icon

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: Component = () => {
     <div class={styles.App}>
       <h1>Dennis Witt</h1>
       <h3>Developer</h3>
-      <FontAwesomeLink identifier='fa-brands fa-github' url='https://github.com/wittdennis' openInNewTab={true} />
+      <FontAwesomeLink identifier='fa-brands fa-github fa-lg' url='https://github.com/wittdennis' openInNewTab={true} />
     </div>
   );
 };
